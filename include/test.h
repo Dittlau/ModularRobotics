27513ae72b224be76140f30f466190d0da2d7b95
Kinna <kinna.mk@gmail.com>
2012-06-06 11:53:21 +0200

    New files

---
+++ include/test.h
@@ -0,0 +1,14 @@
+
+/*
+ * test.h
+ *
+ *  Created on: 04/06/2012
+ *      Author: Kinna
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#endif /* TEST_H_ */
+
+void printFunction();
---
+++ src/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * test.c
+ *
+ *  Created on: 04/06/2012
+ *      Author: Kinna
+ */
+
+void printFunction(){
+	printf("test function called");
+}
+
