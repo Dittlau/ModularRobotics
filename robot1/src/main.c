f7321f944841530d817191ccd6c2e519a9e7d3e6
Dittlau <daniel.dittlau@gmail.com>
2012-06-08 13:52:33 +0200

    new main for project 1

--- robot1/src/main.c
+++ robot1/src/main.c
@@ -1,17 +1,39 @@
-#include <stdio.h>
-#include <avr/io.h>
-#include <util/delay.h>
+/*
+ * Robot_3.c
+ *
+ * Created: 05-06-2012 18:57:21
+ *  Author: Daniel
+ */ 
+
+#include "init.h"
 #include <avr/interrupt.h>
 #include "serial.h"
+#include "dynamixel.h"
+
 
 int main(){
-	int val;
-	serial_initialize(57600);
-	//ADCInit(1);
-	val = 10;//ADCConvert();
-	printf("%d\n\n",val);
+	//portInit();
+	//serial_initialize(57600);
+	dxl_initialize( 0, 1 ); // init with baud = 1 Mbps
+	adcInit();
+	sei(); //Enables global interrupts
+
+	while(1) {
+	
+	// Get sensor reading (in cm)
+	distanceLeft IRdistance(getSensorValue(1));
+	distanceRight IRdistance(getSensorValue(4));
+	
+	// Calculating the required speed
+	speedcalibration = 5;
+	speedLeft = distanceRight*speedcalibration;
+	speedRight = distanceLeft*speedcalibration;
+	
+	// Making the wheels spin (in cm/s)
+	wheel(1,speedLeft);
+	wheel(2,speedRight);
+		
+	}
+
 	return 0;
 }
-
-
-
--- robot3/main.c
+++ robot3/main.c
@@ -10,37 +10,29 @@
 #include "serial.h"
 #include "dynamixel.h"
 
-ISR(TIMER1_COMPA_vect){
-	TCNT1 = 0; // resets timer
-}
-
-//ISR(USART0_RX_vect) {
-	//data = UDR0;
-	//
-//}
-
 
 int main(){
-	portInit();
-	serial_initialize(57600);
+	//portInit();
+	//serial_initialize(57600);
 	dxl_initialize( 0, 1 ); // init with baud = 1 Mbps
-	timerInit();
 	adcInit();
 	sei(); //Enables global interrupts
 
 	while(1) {
-		
-	PORTA &= ~0x80;			// ADC Port 1 IR ON
-	LEDON_MANAGE;
-	printf("Running...");
-	ADCSRA |= (1 << ADIF);		// AD-Conversion Interrupt Flag Clear
-	ADCSRA |= (1 << ADSC);		// AD-Conversion Start
-	while( !(ADCSRA & (1 << ADIF)) );
-	PORTA = 0xFC;
 	
-	LEDOFF_MANAGE;
-	printf("ADC: %d \n",ADC);
-	_delay_ms(500);		
+	// Get sensor reading (in cm)
+	distanceLeft IRdistance(getSensorValue(1));
+	distanceRight IRdistance(getSensorValue(4));
+	
+	// Calculating the required speed
+	speedcalibration = 5;
+	speedLeft = distanceRight*speedcalibration;
+	speedRight = distanceLeft*speedcalibration;
+	
+	// Making the wheels spin (in cm/s)
+	wheel(1,speedLeft);
+	wheel(2,speedRight);
+		
 	}
 
 	return 0;
