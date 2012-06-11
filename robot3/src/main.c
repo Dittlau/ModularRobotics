4d7829e9564fc6c4a566df30e4a2099098991654
Dittlau <s051899@student.dtu.dk>
2012-06-11 22:45:56 +0200

    new

--- robot3/src/main.c
+++ robot3/src/main.c
@@ -4,35 +4,49 @@
  * Created: 05-06-2012 18:57:21
  *  Author: Daniel
  */ 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
-#include <util/delay.h>
+//#include <avr/io.h>
+//#include <avr/interrupt.h>
+//#include <stdio.h>
+//#include <util/delay.h>
 #include "serial.h"
 
 
 
-#define LINK_PLUGIN 0x80
-#define ENABLE_RXD_LINK_PC 0x20
-#define ENABLE_RXD_LINK_ZIGBEE 0x40
+
 
 int main(void)
 {
-	int RcvData;
-	
-PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
-PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
-//PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-//PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
 
-	sei();	// Interrupt Enable
 	
 
 	
 	while(1)
 	{
-			printf("Carrot cake");
+			
 	}
 
 	return 0;
 }
+
+/*
+// ZIGBEE STUFF
+
+#define LINK_PLUGIN 0x80
+#define ENABLE_RXD_LINK_PC 0x20
+#define ENABLE_RXD_LINK_ZIGBEE 0x40
+
+
+	int RcvData;
+	
+	PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
+	PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
+	//PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
+	//PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
+
+	sei();	// Interrupt Enable
+	
+	while(1)
+	{
+		printf("Carrot cake");
+	}
+*/
