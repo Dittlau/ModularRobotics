19c93fe7e02cd72e1cfba7558f4d0505b705c840
Dittlau <daniel.dittlau@gmail.com>
2012-06-11 17:27:37 +0200

    new

--- robot2/src/main.c
+++ robot2/src/main.c
@@ -1,17 +1,49 @@
+/*
+ * Robot_3.c
+ *
+ * Created: 05-06-2012 18:57:21
+ *  Author: Daniel
+ */ 
+#define F_CPU 16000000UL
 #include <stdio.h>
-#include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include "init.h"
 #include "serial.h"
+#include "dynamixel.h"
+#include "adc.h"
+#include "wheel.h"
+
 
 int main(){
-	int val;
+	//portInit();
 	serial_initialize(57600);
-	//ADCInit(1);
-	val = 10;//ADCConvert();
-	printf("%d\n\n",val);
+	dxl_initialize( 0, 1 ); // init with baud = 1 Mbps
+	ADCInit();
+	sei(); //Enables global interrupts
+
+	unsigned int distanceLeft, distanceRight, speedLeft, speedRight, front;
+	unsigned char speedcalibration;
+	while(1) {
+		//printf("%d   %d   %d   %d   %d\n\n",getSensorValue(3),getSensorValue(4),getSensorValue(5),getSensorValue(2),getSensorValue(6));
+		//_delay_ms(1000);
+
+		// Get sensor reading (in cm)
+		front = DMSDistance(getSensorValue(1));
+		distanceLeft = IRDistance(getSensorValue(3));
+		distanceRight = IRDistance(getSensorValue(5));
+
+		// Calculating the required speed
+		speedcalibration = 5;
+		speedRight = (int)(front+2*(distanceLeft-distanceRight));
+		speedLeft = (int)(front+2*(distanceRight-distanceLeft));
+
+		printf("%d   %d\n\n",speedLeft,speedRight);
+	
+		// Making the wheels spin (in cm/s)
+		wheel(1,-speedRight);
+		wheel(2,speedLeft);
+
+	}
+
 	return 0;
 }
-
-
-
--- robot3/src/main.c
+++ robot3/src/main.c
@@ -4,27 +4,34 @@
  * Created: 05-06-2012 18:57:21
  *  Author: Daniel
  */ 
-#define F_CPU 16000000UL
-#include <stdio.h>
+#include <avr/io.h>
 #include <avr/interrupt.h>
-#include "init.h"
+#include <stdio.h>
+#include <util/delay.h>
 #include "serial.h"
-#include "dynamixel.h"
-#include "adc.h"
-#include "legs.h"
 
 
-int main(){
-	//portInit();
-	serial_initialize(57600);
-	dxl_initialize( 0, 1 ); // init with baud = 1 Mbps
-	ADCInit();
-	sei(); //Enables global interrupts
+
+#define LINK_PLUGIN 0x80
+#define ENABLE_RXD_LINK_PC 0x20
+#define ENABLE_RXD_LINK_ZIGBEE 0x40
+
+int main(void)
+{
+	int RcvData;
 	
-	move(FORWARD,1);
-	while(1) {
+PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
+PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
+//PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
+//PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
 
+	sei();	// Interrupt Enable
+	
 
+	
+	while(1)
+	{
+			printf("Carrot cake");
 	}
 
 	return 0;
