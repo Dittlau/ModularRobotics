eaab46ca4263b73d68ef8a9e9605326666572f8d
Dittlau <s051899@student.dtu.dk>
2012-06-12 21:17:31 +0200

    zigbee stuff

--- robot3/src/main.c
+++ robot3/src/main.c
@@ -9,31 +9,7 @@
 #include <stdio.h>
 #include <util/delay.h>
 #include "serial.h"
-
-#define LINK_PLUGIN 0x80
-#define ENABLE_RXD_LINK_PC 0x20
-#define ENABLE_RXD_LINK_ZIGBEE 0x40
-
-
-void serial_set_zigbee();
-void serial_set_wire();
-
-///
-
-void serial_set_zigbee() {
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE); //Output on the 3 pins -MAY NOT BE NESSESARY
-
-  PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
-  PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
-}
-
-void serial_set_wire() {
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE); //-MAY NOT BE NESSESARY
-  
-  PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-  PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
-}
-
+#include "zig.h"
 
 int main(void)
 {
@@ -51,26 +27,3 @@ int main(void)
 
 	return 0;
 }
-
-/*
-// ZIGBEE STUFF
-
-#define LINK_PLUGIN 0x80
-#define ENABLE_RXD_LINK_PC 0x20
-#define ENABLE_RXD_LINK_ZIGBEE 0x40
-
-
-	int RcvData;
-	
-	PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
-	PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
-	//PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-	//PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
-
-	sei();	// Interrupt Enable
-	
-	while(1)
-	{
-		printf("Carrot cake");
-	}
-*/
