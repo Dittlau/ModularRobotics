c47a4ee6e5f226acbc915bfd4372b431bc623405
Dittlau <daniel.dittlau@gmail.com>
2012-06-14 08:31:58 +0200

    gfr

---
+++ firmware/src/zig.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#include "zig.h"
+
+void set_zigbee() {
+
+	//Output on the 3 pins -MAY NOT BE NESSESARY
+	//DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
+
+	PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
+	PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
+
+}
+
+void set_wire() {
+	//-MAY NOT BE NESSESARY
+	//DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE);
+	
+	PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
+	PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
+	
+}
