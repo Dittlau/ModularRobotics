06511684d3877b2b447a606e894708c0df2182a2
Marcellus123 <marcellus0209@yahoo.com>
2012-06-15 11:38:04 +0200

    moved zig initialize etc

--- robot2/src/main.c
+++ robot2/src/main.c
@@ -12,30 +12,8 @@
 #include "serial.h"
 #include "dynamixel.h"
 #include "legs.h"
-
-#define LINK_PLUGIN 0x80
-#define ENABLE_RXD_LINK_PC 0x20
-#define ENABLE_RXD_LINK_ZIGBEE 0x40
-
-
-void serial_set_zigbee();
-void serial_set_wire();
-
+#include "zig.h"
 ///
-
-void serial_set_zigbee() {
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE); //Output on the 3 pins -MAY NOT BE NESSESARY
-
-  PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
-  PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
-}
-
-void serial_set_wire() {
-  DDRD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC | ENABLE_RXD_LINK_ZIGBEE); //-MAY NOT BE NESSESARY
-  
-  PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-  PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
-}
 
 
 int main(void)
@@ -43,7 +21,7 @@ int main(void)
 	int state = 1;
 	
 	serial_initialize(57600);
-	serial_set_zigbee();
+	zigbee_initialize();
 	//dxl_initialize( 0, 1 );
 	sei();
 	legsInit();
@@ -51,17 +29,19 @@ int main(void)
 
 	//dxl_write_word(8,GOAL_POSITION_L,convert(10))
 	
-	printf("%d\n",convert(45));
+	//printf("%d\n",convert(45));
 	
 	
 	while(1)
 	{
-		forward(state,40,40);
+		forward(state,40,20);
+		//forward(5-state,40,40);//backwards
 		state++;
 		if(state > 4) state = 1;
-		//incoming = getchar();
+		
+		/*incoming = getchar();
 		//printf("%d\n",incoming);
-		/*if(incoming == 'w'){
+		if(incoming == 'w'){
 			forward(state,40,40);
 			state++;	
 		}			
@@ -73,33 +53,9 @@ int main(void)
 			state++;
 		}
 						
-		if(state > 4) state = 1;*/
-		
+		if(state > 4) state = 1;
+		*/
 	}
 
 	return 0;
 }
-
-
-/*
-// ZIGBEE STUFF
-
-#define LINK_PLUGIN 0x80
-#define ENABLE_RXD_LINK_PC 0x20
-#define ENABLE_RXD_LINK_ZIGBEE 0x40
-
-
-	int RcvData;
-	
-	PORTD &= ~(LINK_PLUGIN | ENABLE_RXD_LINK_PC); //Deactivate LINK_PLUGIN and ENABLE_RXD_LINK_PC   // no pull up
-	PORTD |= ENABLE_RXD_LINK_ZIGBEE; //Activate ENABLE_RXD_LINK_ZIGBEE
-	//PORTD &= ~(ENABLE_RXD_LINK_ZIGBEE); //Deactivate ENABLE_RXD_LINK_ZIGBEE
-	//PORTD |= LINK_PLUGIN | ENABLE_RXD_LINK_PC; //Activate ENABLE_RXD_LINK_PC
-
-	sei();	// Interrupt Enable
-	
-	while(1)
-	{
-		printf("Carrot cake");
-	}
-*/
